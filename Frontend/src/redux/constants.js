236007c35f6b5b0a15b726c7990269fa178a5539
Hiranjith <ehiranjith@gmail.com>
2024-12-16 11:17:54 +0530

    constants.js in backend folder updated to allow only render api

--- Frontend/src/redux/constants.js
+++ Frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000';
+export const BASE_URL = 'https://shopzee-backend-ww3u.onrender.com' //import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000';
 export const USERS_URL = '/api/users'
 export const CATEGORY_URL ='/api/category'
 export const PRODUCT_URL =  '/api/products'
